82fec17cc161cf013c1884746ea66f16c9b22dad
belal <belalhsoaam@gmail.com>
2025-03-09 19:42:57 +0200

    add my files

--- config/redis.js
+++ config/redis.js
@@ -1,4 +1,4 @@
 const Redis = require("ioredis");
-require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: "config.env" });
 const redis = new Redis(process.env.REDIS_DATABASE_URL);
 module.exports = redis;
